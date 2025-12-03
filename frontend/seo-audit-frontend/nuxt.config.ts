2382a0793975b1f74724b4c1209be711b150f32b
JeanEricH45 <jean.eric@horizons45.com>
2025-12-03 14:20:56 +0800

    fix: ssr asset handling

--- frontend/seo-audit-frontend/nuxt.config.ts
+++ frontend/seo-audit-frontend/nuxt.config.ts
@@ -1,16 +1,11 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
-  // Enable SSR (default)
-  ssr: true,
-
   modules: ["@nuxtjs/tailwindcss"],
 
   css: ["~/assets/css/tailwind.css"],
 
-  app: {
-    head: {
-      title: "SEO Audit Tool",
-    },
-  },
-})
+//   devtools: { enabled: true },
+
+  nitro: {
+    preset: "netlify"
+  }
+});
