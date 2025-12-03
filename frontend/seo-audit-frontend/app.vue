443220c7b611eed7334cda2a6e93691a677b1202
JeanEricH45 <jean.eric@horizons45.com>
2025-12-03 16:43:07 +0800

    fix: switch to tailwind cdn

--- frontend/seo-audit-frontend/app.vue
+++ frontend/seo-audit-frontend/app.vue
@@ -3,3 +3,14 @@
 		<NuxtPage />
 	</NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+	link: [
+		{
+			rel: 'stylesheet',
+			href: 'https://cdn.jsdelivr.net/npm/tailwindcss@3.4.4/dist/tailwind.min.css',
+		},
+	],
+});
+</script>
--- frontend/seo-audit-frontend/nuxt.config.ts
+++ frontend/seo-audit-frontend/nuxt.config.ts
@@ -1,17 +1,7 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   nitro: {
     preset: 'netlify'
   },
 
-  devtools: { enabled: false },
-
-  css: ['~/assets/css/tailwind.css'],
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {}
-    }
-  }
+  devtools: { enabled: false }
 })
